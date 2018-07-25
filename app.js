e929eb1d7e11862ae1c8968009359ff4037daf6a
Herr-Gunter <kubyshkin1990@mail.ru>
2018-07-25 10:35:03 +0300

    fours commit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use( methodOverride('_method') );
 console.log('app has started');
 
 app.set('view engine', 'ejs');
@@ -16,9 +14,6 @@ app.get('/', (req, res) =>{
 app.get('/home', (req, res) =>{
 	res.render('home');	
 });																			
-app.post('/', (req, res) =>{
-	
-});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
